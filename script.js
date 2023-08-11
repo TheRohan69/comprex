573142b72f7fa017ab50f8d5a2e75e4b70e22be9
TheRohan69 <140490884+TheRohan69@users.noreply.github.com>
2023-08-11 16:49:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$( ".change" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+        $( "body" ).removeClass( "dark" );
+        $( ".change" ).text( "OFF" );
+    } else {
+        $( "body" ).addClass( "dark" );
+        $( ".change" ).text( "ON" );
+    }
+});
+
+var navLinks = document.getElementById("navLinks")
+
+        function showMenu(){
+            navLinks.style.right = "0";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        }
